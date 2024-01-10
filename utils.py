c34372359c31fa6662772d2a0b219a14e6b190f6
Gustavo <gustavo_alves2010@yahoo.com.br>
2024-01-10 17:02:02 -0300

    feat: add features engineering comparissons on logistic regression

--- utils.py
+++ utils.py
@@ -16,40 +16,42 @@ def plot_data(X, y, pos_label="y=1", neg_label="y=0"):
     negative = y == 0
 
     plt.plot(X[positive, 0], X[positive, 1], 'k+', label=pos_label)
-    plt.plot(X[negative, 0], X[negative, 1], 'yo', label=pos_label)
+    plt.plot(X[negative, 0], X[negative, 1], 'yo', label=neg_label)
 
-def plot_decision_boundary(w, b, X, y):
+
+def plot_linear_decision_boundary(w, b, X, y):
     # Credit to dibgerge on Github for this plotting code
-     
     plot_data(X[:, 0:2], y)
-    
-    if X.shape[1] <= 2:
-        plot_x = np.array([min(X[:, 0]), max(X[:, 0])])
-        plot_y = (-1. / w[1]) * (w[0] * plot_x + b)
-        
-        plt.plot(plot_x, plot_y, c="b")
-        
-    else:
-        u = np.linspace(-1, 1.5, 50)
-        v = np.linspace(-1, 1.5, 50)
-        
-        z = np.zeros((len(u), len(v)))
 
-        # Evaluate z = theta*x over the grid
-        for i in range(len(u)):
-            for j in range(len(v)):
-                z[i,j] = sig(np.dot(map_feature(u[i], v[j]), w) + b)
-        
-        # important to transpose z before calling contour       
-        z = z.T
-        
-        # Plot z = 0.5
-        plt.contour(u,v,z, levels = [0.5], colors="g")
+    plot_x = np.array([min(X[:, 0]), max(X[:, 0])])
+    plot_y = (-1. / w[1]) * (w[0] * plot_x + b)
+    
+    plt.plot(plot_x, plot_y, c="b")
+
+def plot_decision_boundary(w, b, X, y, degree):
+    # Credit to dibgerge on Github for this plotting code
+    plot_data(X[:, 0:2], y)
+
+    u = np.linspace(-1, 1.5, 50)
+    v = np.linspace(-1, 1.5, 50)
+    
+    z = np.zeros((len(u), len(v)))
+
+    # Evaluate z = theta*x over the grid
+    for i in range(len(u)):
+        for j in range(len(v)):
+            z[i,j] = sig(np.dot(map_feature(u[i], v[j], degree), w) + b)
+    
+    # important to transpose z before calling contour       
+    z = z.T
+    
+    # Plot z = 0.5
+    plt.contour(u,v,z, levels = [0.5], colors="g")
 
 
 import numpy as np
 
-def map_feature(X1, X2, degree = 7):
+def map_feature(X1, X2, degree = 2):
     X1 = np.atleast_1d(X1)
     X2 = np.atleast_1d(X2)
 
